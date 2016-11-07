adaf9e143f55209c12dce91dfbf86ecede42afc5
Liem Ly Quan <liemlyquan@gmail.com>
2016-11-07 20:26:54 +0700

    Initialize Pod

---
+++ CheckInPro/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  CheckInPro
+//
+//  Created by Liem Ly Quan on 11/7/16.
+//  Copyright © 2016 coderschool. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+
+}
---
+++ Pods/Target Support Files/Pods-CheckInPro/Pods-CheckInPro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CheckInPro : NSObject
+@end
+@implementation PodsDummy_Pods_CheckInPro
+@end
